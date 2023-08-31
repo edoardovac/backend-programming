1f038c59764680fa369e70c9d490b127997450aa
edoardovac <edoardo.vacca@hotmail.it>
2023-08-31 11:37:54 +0300

    First commit

---
+++ backend_programming/backend_Programming/src/main/java/com/example/backend_Programming/web/IntroductionController.java
@@ -0,0 +1,26 @@
+package com.example.backend_Programming.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class IntroductionController {
+	@RequestMapping("/index")
+	public String indexPage() {
+		return "This is the main page";
+	}
+
+	@RequestMapping("/contact")
+	public String contact() {
+		return "This is the contact page";
+	}
+
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(name = "location") String location, @RequestParam(name = "name") String name) {
+		return "Welcome to the " + location + " " + name;
+	}
+
+}
---
+++ backend_programming/backend_Programming/src/test/java/com/example/backend_Programming/BackendProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_Programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
